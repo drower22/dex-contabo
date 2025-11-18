7a0bcbfd499d5c7aa3be546cb5067ce235c23b57
Ismar <ismar@pitzei.com.br>
2025-11-18 10:26:44 -0300

    Revert "fix: add User-Agent and Accept headers to iFood API calls"
    This reverts commit fbdb6df3f60bcde4eb1005f934e518b19e4d0672.

--- api/ifood/sales/get.ts
+++ api/ifood/sales/get.ts
@@ -50,8 +50,6 @@ export async function salesGetHandler(req: Request, res: Response) {
       headers: {
         'Authorization': `Bearer ${token}`,
         'Content-Type': 'application/json',
-        'Accept': 'application/json',
-        'User-Agent': 'dex-api/1.0',
       }
     });
 
